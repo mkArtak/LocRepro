0b8d3c060f7dff129136887daac25b3014b88dbb
mkArtak <mkArtak@users.noreply.github.com>
2020-06-26 04:06:42 -0700

    Added the repro project

---
+++ src/LocRepro/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using LocRepro.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LocRepro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/bad")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var item = await Task.Run(() => new object());
+            return View(null);
+        }
+
+        [Route("/good")]
+        public async Task<IActionResult> Index(string id)
+        {
+            var item = await Task.FromResult(new object());
+            return View(null);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            string lang;
+            var langCookie = Request.Cookies["culture"];
+            if (langCookie != null)
+            {
+                lang = langCookie;
+            }
+            else
+            {
+                lang = "am";
+            }
+            SetLanguage(lang);
+            base.OnActionExecuted(context);
+        }
+
+        private void SetLanguage(string lang)
+        {
+            try
+            {
+                var cultureInfo = new CultureInfo(lang);
+                cultureInfo.DateTimeFormat.ShortDatePattern = "dd-MM-yyyy hh:mm:ss";
+                cultureInfo.DateTimeFormat.DateSeparator = "-";
+                Thread.CurrentThread.CurrentUICulture = cultureInfo;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureInfo.Name);
+                CookieOptions option = new CookieOptions();
+                option.Expires = DateTime.Now.AddYears(10);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ src/LocRepro/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Globalization;
+
+namespace LocRepro
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            
+            services.AddLocalization(opts => opts.ResourcesPath = "Resources");
+            services.AddMvc()
+                    .AddViewLocalization(
+                         LanguageViewLocationExpanderFormat.Suffix,
+                         opts => opts.ResourcesPath = "Resources")
+                    .AddDataAnnotationsLocalization().AddRazorRuntimeCompilation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            var supportedCultures = new[]
+           {
+                new CultureInfo("am-AM"),
+                new CultureInfo("en-US")
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
